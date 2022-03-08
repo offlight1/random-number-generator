fb42817f186a11d22af8b08db9b343335e695369
offlight1 <100810134+offlight1@users.noreply.github.com>
2022-03-07 20:16:39 -0500

    Add files via upload
    This is the code for the program!

---
+++ multiclipboard.py
@@ -0,0 +1,13 @@
+#random number generator
+
+import random
+
+print("Use this generator to your liking, it includes a range of numbers from 0 to 10000. it also has a decimal generator with an unlimited range!")
+
+#here it generates a random integer and prints it
+number = random.randint(0,10000)
+print(number)
+
+#now we will repeat this but with float values
+number_decimals = random.random()
+print(number_decimals)
