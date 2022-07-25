cef9ca45912f34f0efc4a75f455a0c0acabeccc3
Lightend <100810134+offlight1@users.noreply.github.com>
2022-07-25 15:12:52 -0400

    I updated the integer range.
    This was a very VERY simple change. I simply extended the range for integers to a billion.

--- multiclipboard.py
+++ multiclipboard.py
@@ -5,7 +5,7 @@ import random
 print("Use this generator to your liking, it includes a range of numbers from 0 to 10000. it also has a decimal generator with an unlimited range!")
 
 #here it generates a random integer and prints it
-number = random.randint(0,10000)
+number = random.randint(0,1000000000)
 print(number)
 
 #now we will repeat this but with float values
